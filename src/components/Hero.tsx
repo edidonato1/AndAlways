2fc09c2445800919542b68c3a496d5136da29b36
Eddie <edward.f.didonato@gmail.com>
2020-12-30 18:21:34 -0500

    style button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,17 +26,17 @@ export default function Hero({ src, data }) {
             <div >
               <Image src={src} width="573" height="767"  layout="intrinsic" quality={100} />
             </div>
-            <button className="bg-yellow-400  w-button text-white py-3 px-5 rounded">{data.cta}</button>
+            <button className="bg-yellow-400 shadow-xl w-button text-white py-3 px-5 rounded">{data.cta}</button>
           </div>
-          <small className="text-center py-5">{data.content}</small>
+          <small className="text-center py-5 text-blue">{data.content}</small>
         </div>
         :
 
-        <div className="flex justify-between w-screen xs:h-5/6 sm:h-1/2 my-20 ">
+        <div className="flex justify-between w-screen xs:h-5/6 sm:h-1/2 my-20 min-h-full">
           <div className="flex xs:flex-col xs:w-3/5 md:w-2/5 justify-evenly h-100 ">
-            <h1 className="text-blue text-4xl font-bold ">{data.header}</h1>
-            <p className="">{data.content}</p>
-            <button className="bg-yellow-400  w-button text-white py-3 px-5 rounded">{data.cta}</button>
+            <h1 className="text-blue text-4xl font-extrabold ">{data.header}</h1>
+            <p className="font-light text-blue sm:text-l md:text-xl">{data.content}</p>
+            <button  className="bg-yellow-400 shadow-xl w-button text-white py-3 px-5 rounded">{data.cta}</button>
           </div>
           <div className="relative h-100 xs:w-3/5 w-2/5" >
             <Image src={src} layout="fill" objectFit="contain" quality={100} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ['./src/components/**/*.{ts,tsx,js,jsx}', './src/pages/**/*.{ts,tsx,js,jsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    minHeight: {
+      'full': '370px',
+    },
+    boxShadow: {
+      xl: "0 8px 15px 4px rgba(228, 189, 15, 0.4), inset 0 -13px 10px 10px rgba(255, 170, 0, 0.7)",
+    },
     screens: {
       'xs': '300px',
       'sm': '600px',
@@ -14,7 +20,7 @@ module.exports = {
         blue: '#324A59',
       },
       width: {
-        'button': '240px',
+        'button': '220px',
       },
     },
   },
