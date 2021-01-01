e6c21561f96e9baa75663f5a6b1a646d149088e1
Eddie <edward.f.didonato@gmail.com>
2021-01-01 11:38:47 -0500

    switch to SSR / move api call to index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { getData } from '../services/hero';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [data, setData] = useState({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const heroData = await getData(); // grabbing data from cms
-      setData(heroData);
-    }
-    fetchData();
-  }, []);
-
-  return <Component data={data}{...pageProps} />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Hero from "../components/Hero";
 
-export default function Home(props) {
-  const { data } = props
+export default function Home({data}) {
+
   
   return (
     <div className="xs:px-0 sm:px-10 container flex items-start mx-auto h-screen">
@@ -11,4 +11,15 @@ export default function Home(props) {
         src="/images/hero.png" />
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:3000/api/cms`)
+  const data = await res.json()
+
+  return {
+    props: {
+      data: data
+    }
+  }
 }
